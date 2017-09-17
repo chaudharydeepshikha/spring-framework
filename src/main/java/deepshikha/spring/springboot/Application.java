8c7519f7fb23e40a792d53e9415ed90b90b77f49
Deepshikha <Deepshikha.Chaudhary@tatacommunications.com>
2017-09-17 20:36:04 +0530

    the spring framework context

--- src/main/java/deepshikha/spring/springboot/Application.java
+++ src/main/java/deepshikha/spring/springboot/Application.java
@@ -1,12 +1,18 @@
 package deepshikha.spring.springboot;
 
+import deepshikha.spring.springboot.controller.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+
+		ApplicationContext context = SpringApplication.run(Application.class, args);
+		MyController myController = (MyController) context.getBean("myController");
+
+		myController.hello();
 	}
 }
---
+++ src/main/java/deepshikha/spring/springboot/controller/MyController.java
@@ -0,0 +1,12 @@
+package deepshikha.spring.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String hello(){
+        System.out.println("Hello world!");
+        return "foo";
+    }
+}
