c5b02918043f9163cda57afed824cdf96c46d959
Deepshikha <Deepshikha.Chaudhary@tatacommunications.com>
2017-09-24 13:32:49 +0530

    Assignment-Display list of Authors (018)

---
+++ spring-boot/src/main/java/deepshikha/spring/framework/springboot/Controller/AuthorController.java
@@ -0,0 +1,22 @@
+package deepshikha.spring.framework.springboot.Controller;
+
+import deepshikha.spring.framework.springboot.Repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+}
