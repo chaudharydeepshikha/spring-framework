ffb416be770ff4104cd6495f5a23f863d6548aaf
Deepshikha <Deepshikha.Chaudhary@tatacommunications.com>
2017-10-01 22:29:37 +0530

    spring boot application properties (068)

--- di-demo/src/main/java/deepshikha/spring/springboot/config/propertyConfig.java
+++ di-demo/src/main/java/deepshikha/spring/springboot/config/propertyConfig.java
@@ -2,23 +2,15 @@ package deepshikha.spring.springboot.config;
 
 import deepshikha.spring.springboot.ExampleBeans.FakeDataSource;
 import deepshikha.spring.springboot.ExampleBeans.FakeJMSBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 /**
  * @author Deepshikha Chaudhary
  */
 @Configuration
-@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
 public class propertyConfig {
-
-    @Autowired
-    Environment environment;
 
     @Value("${didemo.username}")
     String username;
@@ -41,7 +33,7 @@ public class propertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource  dataSource = new FakeDataSource();
-        dataSource.setUsername(environment.getProperty("didemo.username"));
+        dataSource.setUsername(username);
         dataSource.setPassword(password);
         dataSource.setDburl(dburl);
 
@@ -56,10 +48,5 @@ public class propertyConfig {
         jmsBroker.setDburl(jmsDbUrl);
 
         return jmsBroker;
-    }
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties(){
-        return new PropertySourcesPlaceholderConfigurer();
     }
 }
