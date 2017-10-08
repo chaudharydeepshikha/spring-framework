41965da47412cd091c528e264404f92bb95dae8b
Deepshikha <Deepshikha.Chaudhary@tatacommunications.com>
2017-10-09 00:31:46 +0530

    Assignment- one to one JPA Relationship

---
+++ Recipe-application/src/main/java/deepshikha/springframework/recipe/Domain/UnitOfMeasure.java
@@ -0,0 +1,34 @@
+package deepshikha.springframework.recipe.Domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author Deepshikha chaudhary on 10/9/2017
+ */
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+    private String description;
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
