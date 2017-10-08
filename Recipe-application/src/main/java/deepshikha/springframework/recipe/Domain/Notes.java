67414b06295bd37be54e48b28eb380243c2da6f2
Deepshikha <Deepshikha.Chaudhary@tatacommunications.com>
2017-10-08 23:50:02 +0530

    One to one JPA relationships (092)

---
+++ Recipe-application/src/main/java/deepshikha/springframework/recipe/Domain/Notes.java
@@ -0,0 +1,44 @@
+package deepshikha.springframework.recipe.Domain;
+
+import javax.persistence.*;
+
+/**
+ * @author Deepshikha chaudhary on 10/8/2017
+ */
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
